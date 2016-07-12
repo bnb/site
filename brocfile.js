035c27993081bdf0500e85d1b0244535acc66c35
Leo Lamprecht <mindrun@icloud.com>
2016-07-12 15:23:09 +0200

    Image assets added

--- brocfile.js
+++ brocfile.js
@@ -1,10 +1,12 @@
 const mergeTrees = require('broccoli-merge-trees')
 const compileSass = require('broccoli-sass')
 const esTranspiler = require('broccoli-babel-transpiler')
+const funnel = require('broccoli-funnel')
 
 const dirs = {
   scss: 'assets/scss',
   js: 'assets/js',
+  images: 'assets/images'
 }
 
 const styles = compileSass([dirs.scss], 'main.scss', 'styles.css', {
@@ -15,6 +17,8 @@ const scripts = esTranspiler(dirs.js, {
   compact: true
 })
 
-module.exports = mergeTrees([styles, scripts], {
+const images = new funnel(dirs.images)
+
+module.exports = mergeTrees([styles, scripts, images], {
   overwrite: true
 })
