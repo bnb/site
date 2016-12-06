44aa29cd3b3f8c407d62bb12b4f275a08cfff573
Leo Lamprecht <mindrun@icloud.com>
2016-12-06 18:41:17 +0100

    Use next components for external links

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default () => (
   <div>
@@ -29,11 +30,11 @@ export default () => (
     </section>
 
     <nav>
-      <a href="https://medium.com/@leo">Essays</a>
-      <a href="https://github.com/leo">Code</a>
-      <a href="https://twitter.com/notquiteleo">Opinions</a>
-      <a href="https://dribbble.com/notquiteleo">Art</a>
-      <a href="https://instagram.com/notquiteleo">Stupid teenagers</a>
+      <Link href="https://medium.com/@leo">Essays</Link>
+      <Link href="https://github.com/leo">Code</Link>
+      <Link href="https://twitter.com/notquiteleo">Opinions</Link>
+      <Link href="https://dribbble.com/notquiteleo">Art</Link>
+      <Link href="https://instagram.com/notquiteleo">Stupid teenagers</Link>
     </nav>
   </div>
 )
