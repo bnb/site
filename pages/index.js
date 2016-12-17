f54fab85cb6affc52d5c80c551322ce8deaf0e1b
Leo Lamprecht <mindrun@icloud.com>
2016-12-17 23:47:10 +0100

    Improved meta description

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default () => (
       <meta name="twitter:card" content="summary"/>
       <meta name="twitter:creator" content="@lmprht"/>
       <meta name="twitter:title" content="Leo Lamprecht"/>
-      <meta name="twitter:description" content="A teenagers view on web development and intuitive UI design."/>
+      <meta name="twitter:description" content="About a teenager who likes writing code."/>
       <meta name="twitter:image" content="https://leo.im/assets/touch-icon.png"/>
 
       <link rel="mask-icon" href="https://leo.im/assets/flash.svg" color="#000000"/>
