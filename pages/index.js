b33643e504232fc34d5ce7e38c8ad0ab0742fb10
Leo Lamprecht <mindrun@icloud.com>
2016-12-17 23:45:51 +0100

    Group meta tags in a better way

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,13 @@ export default () => (
       <title>Leo Lamprecht</title>
 
       <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+      <meta name="description" content="A teenagers view on web development and intuitive UI design."/>
+
       <meta name="twitter:card" content="summary"/>
       <meta name="twitter:creator" content="@lmprht"/>
       <meta name="twitter:title" content="Leo Lamprecht"/>
       <meta name="twitter:description" content="A teenagers view on web development and intuitive UI design."/>
       <meta name="twitter:image" content="http://leo.im/assets/touch-icon.png"/>
-
-      <meta name="description" content="A teenagers view on web development and intuitive UI design."/>
 
       <link rel="mask-icon" href="http://leo.im/assets/flash.svg" color="#000000"/>
       <link rel="apple-touch-icon" href="/static/images/touch-icon.png"/>
