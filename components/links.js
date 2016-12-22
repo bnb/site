7f63955fc7dd30389935f24af59b0c6bdf8d4db0
Leo Lamprecht <mindrun@icloud.com>
2016-12-22 21:46:58 +0100

    Better link names

--- components/links.js
+++ components/links.js
@@ -4,8 +4,8 @@ import Link from 'next/link'
 
 export default () => (
   <nav>
-    <Link href="https://twitter.com/notquiteleo">Thoughts</Link>
-    <Link href="https://medium.com/@leo">More Thoughts</Link>
+    <Link href="https://twitter.com/notquiteleo">Opinions</Link>
+    <Link href="https://medium.com/@leo">Essays</Link>
     <Link href="https://github.com/leo">Code</Link>
     <Link href="https://dribbble.com/notquiteleo">Visuals</Link>
   </nav>
