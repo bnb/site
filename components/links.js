2494e6a84cc816a72efb73c8df144aa7801b70c8
Leo Lamprecht <mindrun@icloud.com>
2016-12-22 21:18:45 +0100

    Bye Insta

--- components/links.js
+++ components/links.js
@@ -4,10 +4,9 @@ import Link from 'next/link'
 
 export default () => (
   <nav>
-    <Link href="https://twitter.com/notquiteleo">Opinions</Link>
+    <Link href="https://twitter.com/notquiteleo">Thoughts</Link>
+    <Link href="https://medium.com/@leo">More Thoughts</Link>
     <Link href="https://github.com/leo">Code</Link>
-    <Link href="https://medium.com/@leo">Essays</Link>
-    <Link href="https://dribbble.com/notquiteleo">Art</Link>
-    <Link href="https://instagram.com/notquiteleo">Stupid teenagers</Link>
+    <Link href="https://dribbble.com/notquiteleo">Visuals</Link>
   </nav>
 )
