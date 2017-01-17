0c7b881b8a240728e23a2301897927d112f4f20b
Leo Lamprecht <mindrun@icloud.com>
2017-01-17 17:34:01 +0100

    The Role of Languages in Today’s Web

---
+++ pages/2016/the-role-of-languages.js
@@ -0,0 +1,67 @@
+// Layouts
+import Post from '../../layouts/post'
+
+// Components
+import P from '../../components/paragraph'
+import Meta from '../../components/meta'
+import {Image} from '../../components/figure'
+
+export default () => (
+  <Post>
+    <Image src="/static/posts/the-role-of-languages/cover.png" />
+    <Meta title="The Role of Languages in Today’s Web" date="08-08-2016" />
+
+    <P>We’re living in a global world. Everybody is
+    connected to everyone through the internet and its broad
+    variety of products which want you to spend even more time with it.</P>
+
+    <P>Sure, something like the internet needs standards. There are
+    many many people out there contributing to it every day, from thousands
+    of countries all over the earth. There is definitely
+    a need for a global language which can be understood
+    by everyone. And this is English.</P>
+
+    <P>It’s an easy language — compared to other ones. It’s the
+    language on which most programming languages are based on. It’s also outside
+    the internet, nearly everybody knows it.</P>
+
+    <P>While developing products, setting up wikis and GitHub
+    repositories, I often came across that one question: Every “creator” on the web
+    knows English, so why don’t I just write everything in
+    this language? It would be easy for me and well understandable
+    by developers and designers.</P>
+
+    <P>But in my opinion, that’s not how we should deal with this topic. Language
+    isn’t just another way how we communicate with other
+    individuals around us. It’s also a part of our culture. Century over
+    century, human beings formed their speech to easily communicate
+    information to other humans. Even if they didn’t do it
+    intentionally, they put a huge amount of their livetime into shaping
+    each language until it became as easy as it’s today to transmit
+    thoughts into other people’s minds using written words or speech.</P>
+
+    <P>And of course that’s not the end. We’re constantly
+    changing the way how we speak and write to fit the
+    strain and speed of our everyday life.</P>
+
+    <P>Every language is beautiful. Currently, there
+    are over <b>3.500</b> languages spoken across the world. And in my view, we should
+    keep <b>all of them</b>. As humans — especially designers and
+    developers, we often tend to make everything as perfect and easy as possible.</P>
+
+    <P>But in this case, that’s not helpful at all.</P>
+
+    <P>Of course, it’s easier to just write everything in one
+    language: Everyone could then understand it. But with
+    this method, we’ll loose a huge part of our culture. And culture
+    is what defines us. It’s what we can use to find
+    out what we are. Without it, we would all be the same.</P>
+
+    <P>So don’t support this thinking. You don’t need to translate
+    everything into your mother tongue now, but if you have
+    enough time, contribute something to human existence and also
+    provide another translation in your native language.</P>
+
+    <P>Thank you for giving a fuck! 😘</P>
+  </Post>
+)
