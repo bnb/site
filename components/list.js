7191524a0e4d2ab01e0fdca5c5dfea2b7e93cbeb
Leo Lamprecht <mindrun@icloud.com>
2017-01-17 13:27:12 +0100

    Removed styles from list elements

--- components/list.js
+++ components/list.js
@@ -1,38 +1,13 @@
 const OL = ({ children }) => (
-  <ol>
-    { children }
-    <style jsx>{`
-      ul {
-        margin: 0 0 20px 0;
-        padding: 0;
-        list-style: inside decimal;
-      }
-    `}</style>
-  </ol>
+  <ol>{ children }</ol>
 )
 
 const UL = ({ children }) => (
-  <ul>
-    { children }
-    <style jsx>{`
-      ul {
-        margin: 0 0 20px 0;
-        padding: 0;
-      }
-    `}</style>
-  </ul>
+  <ul>{ children }</ul>
 )
 
 const LI = ({ children }) => (
-  <li>
-    { children }
-    <style jsx>{`
-      li {
-        margin-bottom: 5px;
-        line-height: 24px;
-      }
-    `}</style>
-  </li>
+  <li>{ children }</li>
 )
 
 export { OL, UL, LI }
