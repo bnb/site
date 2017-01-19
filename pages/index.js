c9c57cff47984503d1428435f1fdc7830e2b96da
Leo Lamprecht <mindrun@icloud.com>
2017-01-19 12:23:43 +0100

    Show list of posts on front page

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,39 @@
-// Packages
+// Native
 import Head from 'next/head'
+import React from 'react'
+
+// Packages
+import moment from 'moment'
 
 // Components
 import Link from '../components/link'
 
-const posts = [
-  {
-    id: 1,
-    url: 'http://google.com',
-    title: 'LOOL'
-  }
-]
+// Data
+import posts from '../data/posts'
 
-export default () => (
-  <div>
-    <Head>
-      <title>Leo Lamprecht</title>
-    </Head>
-
-    <div>test</div>
-
-    {
-      posts.map(post => (
-        <Link href={post.url} key={post.id}>
-          {post.title}
-        </Link>
-      ))
+export default class Meta extends React.Component {
+  render() {
+    for (const post of posts) {
+      const year = moment(post.date, 'DD-MM-YYYY').year()
+      post.url = `/${year}/${post.id}`
     }
-  </div>
-)
+
+    return (
+      <div>
+        <Head>
+          <title>Leo Lamprecht</title>
+        </Head>
+
+        <div>test</div>
+
+        {
+          posts.map(post => (
+            <Link href={post.url} key={post.id}>
+              {post.title}
+            </Link>
+          ))
+        }
+      </div>
+    )
+  }
+}
