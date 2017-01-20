9e402346d0d056f58c7a53ae17bc20497fe20d46
Leo Lamprecht <mindrun@icloud.com>
2017-01-20 18:59:18 +0100

    Dynamically create components for headings

--- components/heading.js
+++ components/heading.js
@@ -4,31 +4,51 @@ import React from 'react'
 // Packages
 import toID from 'to-id'
 
-const H1 = class Heading extends React.Component {
-  render() {
-    const content = this.props.children
-    const id = toID(content)
+const H = ({ level, fontSize, linked, children }) => (
+  <div>
+    {
+      React.createElement(`h${level}`,
+        {
+          style: {
+            fontWeight: 500,
+            fontSize
+          }
+        },
+        linked && <span>
+          <a href={`#${toID(children)}`} id={toID(children)}>#</a>
+        </span>,
+        children
+      )
+    }
 
-    return React.createElement(
-      'h' + this.props.level,
-      {
-        style: {
-          fontWeight: 500,
-          fontSize: this.props.fontSize
-        }
-      },
-      <a href={`#${id}`} id={id}>#</a>,
-      content
-    )
-  }
+    <style jsx>{`
+      span {
+        position: absolute;
+        margin-left: -15px;
+        width: 15px;
+      }
+
+      a {
+        visibility: hidden;
+      }
+
+      div:hover a,
+      span:hover a {
+        visibility: visible;
+      }
+    `}</style>
+  </div>
+)
+
+for (let level = 1; level <= 6; level++) {
+  const tag = `H${level}`
+
+  const component = props => new H({
+    ...props,
+    level,
+    fontSize: 20,
+    linked: level > 1
+  })
+
+  exports[tag] = component
 }
-
-H1.defaultProps = {
-  level: 1,
-  fontSize: 16
-}
-
-const H2 = H1
-const H3 = H1
-
-export {H1, H2, H3}
