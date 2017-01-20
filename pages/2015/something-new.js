b8c58b4a3ea1a70c8b8f65a973c5565614f39899
Leo Lamprecht <mindrun@icloud.com>
2017-01-20 22:21:06 +0100

    Something New

---
+++ pages/2015/something-new.js
@@ -0,0 +1,46 @@
+// Layouts
+import Post from '../../layouts/post'
+
+// Components
+import P from '../../components/paragraph'
+import Link from '../../components/link'
+import Meta from '../../components/meta'
+
+export default () => (
+  <Post>
+    <Meta id="something-new"/>
+
+    <P>Finally! It{`'`}s been a while since I{`'`}ve blogged properly
+    the last time, probably a few years. Why? Well, during this
+    time many things changed for me: After I
+    left <Link href="http://cuttingegg.de">Cutting Egg</Link> a
+    year ago, I started working in a  advertising
+    agency called <Link href="https://www.medienreaktor.de">medienreaktor</Link> in addition to
+    school (where I{`'`}ll also graduate in a few months). I{`'`}m glad to
+    finally get a solid reward for making great websites
+    and -apps. Of course, the local cooperation with other people
+    interested in my field also helps me to expand my knowledge.</P>
+
+    <P>Meanwhile, about 5-6 years have passed since I started
+    disassembling random websites to get to know how they work
+    on the dark side of the web (<i>hihi</i>, {`'dark side'`} - sounds
+    scary 👻). I{`'`}m happy that I{`'`}m now able to make websites for
+    people who like my work and want to support me and I really like
+    doing it - of course, it{`'`}s sometimes at bit exhausting, but
+    I think for the long haul, it{`'`}s perfect for me.</P>
+
+    <P>But that{`'`}s just the beginning. After school, I{`'`}ll probably
+    do an apprenticeship for a couple of years to get to
+    know more people in this scene. I also might build some of the
+    ideas that I{`'`}ve sketched in my notebook as I{`'`}ll hopefully
+    have more time for things like that. This is where my
+    blog comes in: I{`'`}d like to build some <b>more connections</b> over
+    the web and I{`'`}ll of course also write some more posts
+    about handling things the right way & my maybe also about
+    my second love: HipHop.</P>
+
+    <P>So if you want to join me on my journey through the world, you{`'`}re free
+    to hit the {`'Follow'`}-button on my Twitter-Account
+    anytime - you can even try the RSS-Feed.</P>
+  </Post>
+)
