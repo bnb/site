6c5caf7b19d5d020e5756fef8893a113418d6e87
Leo Lamprecht <mindrun@icloud.com>
2017-01-21 11:57:49 +0100

    Of Course I'm Dumb, You're Too.

---
+++ pages/2015/of-course-im-dumb.js
@@ -0,0 +1,42 @@
+// Layouts
+import Post from '../../layouts/post'
+
+// Components
+import P from '../../components/paragraph'
+import Meta from '../../components/meta'
+import HR from '../../components/hr'
+
+export default () => (
+  <Post>
+    <Meta id="of-course-im-dumb"/>
+
+    <P>{`"`}Are you stupid?{`"`} — Well, in some cases, I am. In some
+    others, I{`'`}m not. But I definitely know that you
+    are. Why? Because you{`'`}re asking me such a dumb question.</P>
+
+    <P>If a person is {`"`}dumb{`"`} or not, depends on the intellect of the
+    person who{`'`}s judging. If you ask Elon Musk if Homer Simpson is
+    more dumb than him, he will probably answer you with <i>Yes</i>. But if
+    you ask a person who{`'`}s more dumb than Homer Simpson (I{`'`}m not
+    sure if that{`'`}s even possible), she will definitely answer you
+    with <i>No</i> (at least if she{`'`}s being honest). To cut a
+    long story short: The state {`"`}dumb{`"`} is relative to the
+    judging person{`'`}s intellect. More clearly: If you{`'`}re asking
+    a person about being {`"`}dumb{`"`} in a non-rhetorical
+    context, you{`'`}re the dumb one.</P>
+
+    <P>So now tell me, how should I know by myself if I{`'`}m dumb or
+    not? Right, I can{`'`}t! I would have to compare
+    my own state of intellect with itself. And you should know
+    that comparing things with themselves is technically
+    not possible. But since you{`'`}ve asked me that
+    question, you don{`'`}t. Therefore I{`'`}m free to call you dumb.</P>
+
+    <HR/>
+
+    <P>If someone asks you if you{`'`}re dumb, you{`'`}re free to use the first
+    paragraph as an answer. But beware: If the person who{`'`}s asking
+    you is smart enough, she will answer with {`"`}It{`'`}s a
+    rhetorical question, you moron.{`"`}</P>
+  </Post>
+)
