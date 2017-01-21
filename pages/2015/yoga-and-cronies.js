bdc2595dba3330cecd234f6dd8bf9421c7406cf9
Leo Lamprecht <mindrun@icloud.com>
2017-01-21 12:18:30 +0100

    Yoga and It’s Cronies

---
+++ pages/2015/yoga-and-cronies.js
@@ -0,0 +1,41 @@
+// Layouts
+import Post from '../../layouts/post'
+
+// Components
+import P from '../../components/paragraph'
+import Meta from '../../components/meta'
+
+export default () => (
+  <Post>
+    <Meta id="yoga-and-cronies"/>
+
+    <P>As you read this, you will probably already
+    know, what I mean with that word. I strongly believe you
+    also have some friends or family doing it.</P>
+
+    <P>First, let{`'`}s quickly get to know what Yoga even is. According
+    to Wikipedia, it{`'`}s a »physical, mental, and spiritual
+    practice or discipline« which is - in our society - mostly used
+    by people who want to focus and leave their daily
+    stress and work-pressure behind.</P>
+
+    <P><b>That{`'`}s exactly what worries me.</b> I certainly don{`'`}t think
+    that Yoga is a bad thing at all, when done in the right
+    way. It helps you to be more happy and that{`'`}s great, right? So
+    I don{`'`}t have a problem with the thing itself, I have a {`"`}problem{`"`} with
+    the reason why most (not all) people are choosing to do it.</P>
+
+    <P>I think they’re trying to get away from things they don’t
+    like. Instead of facing the truth and doing something to improve
+    the situation and stepping forward, they just want to
+    fade it out. And that is - in my opinion - not useful when developing
+    a great life. They’re imagining that they’re just
+    ameliorating their health and mind - but what’s really happening
+    is that they’re escaping from the bad side of their life.</P>
+
+    <P>That was a bit confusing, huh? I hope you understand, what I
+    want to communicate you: Just put all your effort into
+    creating the life you want and don’t flee from those decisions (of
+    course just a suggestion, I don{`'`}t want to be a preacher 😬).</P>
+  </Post>
+)
