d09d16d7e4c13e9d61aaa1ac23d5d0e7dd1e62a1
Leo Lamprecht <mindrun@icloud.com>
2017-01-22 17:27:49 +0100

    Prepare for good looking snippets

--- components/code.js
+++ components/code.js
@@ -2,8 +2,15 @@
 import Highlight from 'react-syntax-highlighter'
 import {github} from 'react-syntax-highlighter/dist/styles'
 
+const styles = {
+  margin: '30px 0',
+  padding: '15px 10px'
+}
+
 const Snippet = ({ type, children }) => (
-  <Highlight language={type} style={github}>{ children }</Highlight>
+  <Highlight language={type} style={github} customStyle={styles}>
+    { children }
+  </Highlight>
 )
 
 const Inline = ({ children }) => (
