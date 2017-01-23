cf5d8b08c14aee58811dda499c79f1b565b976e3
Leo Lamprecht <mindrun@icloud.com>
2017-01-23 17:37:56 +0100

    FrewHost Wechselt Den Betreiber

---
+++ pages/2013/frewhost-verkauft.js
@@ -0,0 +1,46 @@
+// Layouts
+import Post from '../../layouts/post'
+
+// Components
+import P from '../../components/paragraph'
+import Link from '../../components/link'
+import Meta from '../../components/meta'
+
+export default () => (
+  <Post>
+    <Meta id="frewhost-verkauft"/>
+
+    <P>Mittlerweile ist schon mehr als ein Jahr vergangen, seitdem
+    mein Projekt {`"`}FrewHost{`"`} offiziell ins Netz ging. Über
+    diese Zeit hatte ich natürlich die Möglichkeit
+    Erfahrungen zu sammeln, und diese ins Projekt einzubinden.</P>
+
+    <P>Trotz der vielen Arbeit, die ich und meine Helfer in
+    das Projekt gesteckt haben, kann ich dem Gerücht nur
+    zustimmen, dass FrewHost nun den Besitzer und somit
+    auch den Betreiber wechselt.</P>
+
+    <P>Es hat mir sehr viel Spaß gemacht, bei einem solchen Projekt
+    mitzuwirken, allerdings benötige ich immoment mehr Zeit für
+    mein privates Leben. Weitergehend benötigt FrewHost - wie
+    auf der offiziellen Webseite schon angekündigt - auch mal
+    wieder frischen Wind.</P>
+
+    <P>Selbstverständlich bleiben allen bestehenden Kunden
+    ihre Leistungen erhalten, gegebenenfalls wird das Angebot
+    nur weiter erweitert, bzw. die Produktvielfalt gesteigert. Dies
+    alles liegt aber nun in der Macht
+    von <Link href="mailto:franc@kabelmail.de">Franc Streicher</Link> welcher
+    das Projekt FrewHost absofort verwalten wird.</P>
+
+    <P>Möglicherweiße werde ich bei einigen kommenden Neuerungen
+    noch mitwirken, jedoch ziehe ich mich nun mit hoffen auf euer
+    Verständnis offiziell gesagt aus dem Projekt FrewHost zurück
+    und danke allen Kunden und Helfern dass sie FrewHost zu dem
+    gemacht haben, was es jetzt ist.</P>
+
+    <P>Weitergehend wünsche ich allen bestehenden Kunden - und
+    denen die es noch werden möchten - viel Spaß und
+    Erfolg mit FrewHost.</P>
+  </Post>
+)
