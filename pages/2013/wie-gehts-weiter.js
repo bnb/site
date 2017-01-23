111c60b3c22089e6378dd84018497350cb5004aa
Leo Lamprecht <mindrun@icloud.com>
2017-01-23 17:29:29 +0100

    Wie Geht's Weiter?

---
+++ pages/2013/wie-gehts-weiter.js
@@ -0,0 +1,40 @@
+// Layouts
+import Post from '../../layouts/post'
+
+// Components
+import P from '../../components/paragraph'
+import Link from '../../components/link'
+import Meta from '../../components/meta'
+
+export default () => (
+  <Post>
+    <Meta id="wie-gehts-weiter"/>
+
+    <P>Einige werden sich mittlerweile sicherlich fragen: {`"`}Was
+    macht denn der Leo eigentlich nun?{`"`}. Naja, leicht zu
+    erklären: Nachdem ich neben Medientipps jetzt auch
+    noch <Link href="https://www.frewhost.net/">FrewHost</Link> abgegeben
+    habe, hab ich nun endlich mehr Zeit für andere Hobbys, etc.</P>
+
+    <P>Das Ganze ist jedoch absolut nicht falsch zu
+    verstehen - denn im Web aktiv bleibe ich trotzdem. Ich werde
+    weiterhin mit den zwei Felix und Kevin
+    von <Link href="http://cuttingegg.de">Cutting Egg</Link> Webprojekte
+    entwickeln und zudem anderweitige Projekte wie den lang ersehnten Uptime-Monitor vorantreiben.</P>
+
+    <P>In meiner verbleibenden Freizeit werde ich mich zudem auf
+    die Entwicklung simpler Wordpress-Themes für Blogs
+    konzentrieren, welche ich dann hier auf meiner Webseite präsentieren werde.</P>
+
+    <P>Natürlich stehe ich auch gerne weiterhin für externe
+    Freelance-Arbeiten wie z.B. das Entwickeln und Umsetzen von Front-End-Konzepten
+    zur Verfügung. Hierzu ist nur eine
+    kurze <Link href="mailto:mindrun@icloud.com">E-Mail</Link> vonnöten - im
+    Grunde bin ich in diesem Bereich ziemlich flexibel.</P>
+
+    <P>Weitere Infos und Aktuelles gibt{`'`}s natürlich wie immer mittels
+    RSS-Feed direkt in die Zentrale. Ansonsten freue ich mich auch
+    immer über neue Follower
+    auf <Link href="https://twitter.com/notquiteleo">Twitter</Link>. Stay Egged!</P>
+  </Post>
+)
