e39a0962b7cd3871014ac3af79bb73c5d91a6208
Leo Lamprecht <mindrun@icloud.com>
2017-01-23 17:48:38 +0100

    Besuch Auf Den World Hosting Days 2013

---
+++ pages/2013/whd-2013.js
@@ -0,0 +1,42 @@
+// Layouts
+import Post from '../../layouts/post'
+
+// Components
+import P from '../../components/paragraph'
+import Link from '../../components/link'
+import Meta from '../../components/meta'
+import Quote from '../../components/quote'
+
+export default () => (
+  <Post>
+    <Meta id="whd-2013"/>
+
+    <P>Zunächst einmal vielen herzlichen Dank
+    an <Link href="http://www.technik-aktuell.de/">Max Brown</Link>, der
+    mir diese Aktion überhaupt möglich gemacht hat. Ich werde mit
+    zwei Kollegen auf den WordHostingDays 2013 im Europa-Park
+    Rust (nähe Frankfurt) sein dürfen.</P>
+
+    <P>Dort möchte ich im Autrag meiner Firma ein paar
+    geschäftliche Kontakte schließen, vielleicht sogar einen
+    kleinen Partnervertrag mit Unternehmen wie DENIC
+    abschließen, was für FrewHost natürlich von Vorteil währe.</P>
+
+    <P>Weitergehend möchte ich weitere Informationen über die
+    Hosting-Szene sammeln, und mein Wissen so weit wie
+    möglich ausbauen. Das Event findet demnächst vom 19. bis
+    zum 21. März statt. Wir werden in der Nähe womöglich
+    ein kleines Hotelzimmer buchen, damit wir auch wirklich
+    nichts verpassen.</P>
+
+    <Quote author="Thomas Strohe, WHD Gründer">With more than 100 exhibitors
+    and media partners as well as over 4,200 visitors the WHD.global 2012
+    event was even larger and more diverse than previous
+    years. WHD.global 2013 will be an event not to miss...</Quote>
+
+    <P>Selbstverständlich werde ich über meinen dortigen Aufenthalt in
+    meinem Blog berichten. Also am Besten gleich mal ein Lesezeichen
+    im Browser setzen, falls dieses noch nicht vorhanden sein
+    sollte. So verpassen sie absolut nichts.</P>
+  </Post>
+)
