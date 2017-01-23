7eaa1cace0dc6654e8d6f3ffb81a9b7d94b498a1
Leo Lamprecht <mindrun@icloud.com>
2017-01-23 17:07:28 +0100

    Ohai, Berlin

---
+++ pages/2015/ohai-berlin.js
@@ -0,0 +1,75 @@
+// Layouts
+import Post from '../../layouts/post'
+
+// Components
+import P from '../../components/paragraph'
+import Meta from '../../components/meta'
+import Link from '../../components/link'
+import {H2} from '../../components/heading'
+import {Ref, FootNotes, Note} from '../../components/footnotes'
+
+export default () => (
+  <Post>
+    <Meta id="ohai-berlin"/>
+
+    <P>When I heard of that whole conference movement a while ago, I was firstly a bit confused about why some people are spending their time organizing events on which all kinds of folks from the internet meet together and talk with each other. Every time someone from those big players in our industry decided to post a picture of himself sitting within the crowd of such a conference, I simply couldn't understand it. We're the internet. If we want to talk with people, it doesn't matter if they're on the back side of the world, we just need to hit a button and we're immediately able to both hear and see each other and discuss whatever the fuck we would like to discuss.</P>
+
+    <P>But when I got older, I realized that this way of communicating can't be compared to the way how people deal with each other on those events. It also feels much less forced and freer. There are no connection losses or bad interfaces in between, just a nice bunch of people talking about their favorite topic.</P>
+
+    <P>Since I'm just a human and therefore much more of a fan of this than chatting with someone over the internet, I decided that I have to check out one of those conferences at least once. So I tried to find one which is interesting but sadly soon forgot about it, because I couldn't find a suitable one. But then, after a long time had passed, I noticed on Twitter that there was one about JavaScript starting in Berlin. And since I already really liked JS, I brought myself to do it.</P>
+
+    <P>Now there was only one problem which was standing between me and the conference: Money. I don't know exactly why, but I thought a ticket for it would cost around 100 euros and then turned out to be over 700 (or something like that). And then there was also the cost for the train ticket and my accommodation for three nights. All in all, it was a bit over a thousand euros for just two days of talks and All You Can Eat Food. It was just too much. But as soon as I got sad about that I probably wouldn't be able to attend the event, their Twitter account tweeted about how easy it is to get a scholarship if you're part of a small, ethnic group. I wasn't, so I got sad again.</P>
+
+    <P>Thankfully, they told me that they could even sponsor "regular" people like me if I couldn't afford it. So on the same day, I sent out an application through their web form and hoped that I would be selected as one of the small group of people which will receive a sponsorship.</P>
+
+    <P>Then one day, I got an email with "Congrats, you're going to Berlin!" in the subject line. It was from Simone, one of the great people who spent their time organizing those huge meetups. And then it started, I got really nervous immediately. I knew I will meet new people who're indeed on the same topic as I, but definitely on a different level. But I tried to tell myself to stay calm and booked a nice little room on Airbnb and packed by stuff for the 4-hour trip to Berlin.</P>
+
+    <P>It was a long, exhausting ride on a train with really bad WiFi and omnipresent nausea. But I've managed to distract myself with music and the development of a website which will launch soon<Ref id="1"/>.</P>
+
+    <P>As I finally arrived there, I was immediately blown away by all those huge buildings, huge crowds of business people in grey suits rushing to work and students sitting on benches while looking strained into books, drinking Starbucks coffee and gulping down all kinds of fast food. All in all it didn't felt like a cozy town to me. But much more like a instrument of power which was purely created to fit a huge amount of people in one place and impress other countries.</P>
+
+    <P>It was definitely the result of the megalomania of Germany's past politician that I felt like I was being crushed by endlessly high castles and blocks made of pure concrete and glass.</P>
+
+    <P>Maybe the bombing in the second world war and the thereupon as-fast-as-possible created buildings played a part in this.</P>
+
+    <P>After walking a bit, a huge quadrangular block made of pure concrete and glass showed up in front of me. I looked on the sign that tells the walkers what is being build here and it said something like "The new Berlin castle".</P>
+
+    <P>I took a big step away from the fence, stared at the lifeless built and thought "That's not a castle, you must be kidding me". Then I walked a little further and found another sign (more like a plastic poster) which showed the number 60 written in red letters. Under it, there was a text in a different font which said "Current liabilities of the city in billions".</P>
+
+    <P>Yeah, sure. "Guys, we haven't spend enough of our country's money yet. Let's build a crappy looking concrete block for another few millions!". I guess that pretty much sums up my feelings about Berlin. There are definitely some great spots, but most if it is just not my thing.</P>
+
+    <H2>The Conference</H2>
+
+    <P>Now enough with that talking about the city itself. While I was there, I didn't just sit around for 4 days, I actually did something. The main reason why I even visited Berlin was because of my first web conference, the <Link href="http://2015.jsconf.eu/">JSConf EU</Link>. It's a nice event with great food and friendly people who're presenting cool things that are happing around JavaScript.</P>
+
+    <P>Before the first day of the event, I was very nervous and went to bed very late. In the morning, I woke up early too, just to notice that I haven't brought my hairdryer with me. Fuck. Once I had figured out how to towel them down, the event had already started and I prayed that the breakfast is still going. Thankfully, I had a few minutes to enjoy my pancakes with jam, before the halls got opened for the audience.</P>
+
+    <P>Then, the whole room suddenly got completely darkened. The only things that were still visible were the bright-shining screens of those people, who decided not to close their MacBooks for a fucking second. I knew that they would come up with a great opening, but I did neither expect <Link href="https://vimeo.com/140427080">this</Link>, nor <Link href="http://youtu.be/lJ1kY-CSpBk">that</Link>.</P>
+
+    <P>After the beginning, the crew introduced themselves and the talks started. It was a great journey through all the areas, in which JavaScript is now being used to create great things. I felt like they really took the whole "JavaScript everything" as an attitude for life. I heard of a sewing machine running it and Pawel Szymczykowski showed us how to build a balloon carrying a payload of sensors and cameras which will by launched into the Earth’s atmosphere, all thanks to node.</P>
+
+    <P>I don't want to dig too much into all the great stuff that was shown to us, because all of them will be released in the next days on their <Link href="https://www.youtube.com/user/jsconfeu/videos">YouTube channel</Link>. I'm sadly now able to tell you my favorite talk, because most of them were just above ground. But one of those was definitely "Illusion of Time" by Denys Mishunov (where he explained why people don't care about waiting if they're able to move forward while they wait and why they do care if they need to wait without doing anything) and "Disconnected Networking" by Razvan Caliman. In this one, he showed us how to use various APIs (like audio or ambient light) of the newer browser versions to transfer data between devices or simply control a computer with gestures.</P>
+
+    <P>Another thing I was very happy about was, that Microsoft finally decided to kill the Internet Explorer in favor of their new browser: Edge. They dropped a huge amount of old code lines and replaced them with all the cool features which browsers like Chrome already have since a long time. When I got back to my agency, I had the chance to install Windows 10 on one of our testing devices and I noticed that they haven't completely removed IE yet. But I'm sure they will, in the near future.</P>
+
+    <P>I also met David Valdman, the creator of the talk "Functional Layout - Updating the DOM at 60FPS" (which happened in a different hall at the same time) and we were chatting a bit about how big Berlin is and that it's not comparable to all the beatiful cities we actually have in Germany. Thanks to him, I'm now also aware of how much projects like JSConf do to get some decent talks for their audience. No matter where you live, they will pay for your flight, your accommodation and for all your human needs (food, drinks, et cetera).</P>
+
+    <P>All in all, it was a really great event. Altought I didn't really know where to go at first, I found it out a short while later because everything was so perfectly organized and explaned through signs and the crew. Someone even built a <Link href="http://jsconfeu.monometric.net">full mobile version</Link> of the schedule, so it was very easy to switch halls if the current talk wasn't very interesting.</P>
+
+    <P>Oh, I forgot to mention that: The whole event took place in two different rooms: The "Back track" (where I was most of the time) and the "Side track". Everyone was allowed to change to the other room if the talk on the other side was more appealing to him, or just for fun.</P>
+
+    <P>Sadly, I wasn't able to stay until the Closing Conference and the Group Foto. Since I didn't knew the schedule when I booked my tickets, I bought one of those for the train which can only be used at a certain time. I was so thankful that I've received the sponsorship, but I still wanted to find the cheapest ticket so that I could spend more money on the Airbnb room (which was <Link href="https://www.airbnb.de/rooms/7643348">this one</Link>, by the way). I was very late when I've decided to book a room on Airbnb instead of one in a hotel, so there where only a few ones left.</P>
+
+    <P>But as you can see, I got a great one anyway - it was beatiful!</P>
+
+    <H2>tl;dr</H2>
+
+    <P>If you have the chance to attend one of those web conferences, do it! You will meet a lot of great people, stay up-to-date with the new stuff in our industry and also have a lot of fun!</P>
+
+    <P>On of the biggest problems I personally had was simply being too shy to do it. But trust me, those people are a lot nicer than the the average person you'll meet on the street!</P>
+
+    <FootNotes>
+      <Note id="1">Really, what else?</Note>
+    </FootNotes>
+  </Post>
+)
