f4f6ea0479b465be1a35e954b210c27e1ff651e9
Leo Lamprecht <mindrun@icloud.com>
2017-01-29 22:27:01 +0100

    Make site responsive

--- layouts/page.js
+++ layouts/page.js
@@ -7,6 +7,8 @@ export default ({ children }) => (
       <link rel="mask-icon" href="http://leo.im/static/lightning.svg" color="#000000"/>
       <link rel="apple-touch-icon" href="/static/touch-icon.png"/>
       <link rel="icon" href="/static/touch-icon.png" type="image/png"/>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
     </Head>
 
     {children}
