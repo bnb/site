dc9bae793c231648c7b547ea2d667da000ed7176
Leo Lamprecht <mindrun@icloud.com>
2017-02-10 12:12:22 +0100

    Better ordered lists

--- components/list.js
+++ components/list.js
@@ -1,5 +1,15 @@
 const OL = ({ children }) => (
-  <ol>{ children }</ol>
+  <ol>
+    { children }
+
+    <style jsx>{`
+      ol {
+        list-style: decimal;
+        margin: 30px 0;
+        padding: 0 0 0 22px;
+      }
+    `}</style>
+  </ol>
 )
 
 const UL = ({ children }) => (
@@ -23,7 +33,7 @@ const LI = ({ children }) => (
     <style jsx>{`
       li {
         font-size: 15px;
-        margin-bottom: 20px;
+        margin-bottom: 15px;
         line-height: 23px;
       }
     `}</style>
